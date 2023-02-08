e0a6df81683f5d0af117ba921643a9db101bd5d1
RuslanSharipov63 <sharipov.r@mail.ru>
2023-02-08 16:07:59 +0400

    size image in itempage

--- src/itempage/ItemPage.js
+++ src/itempage/ItemPage.js
@@ -1,15 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { getImageSize } from 'react-image-size';
+
 import Button from '../components/button/Button';
 import style from './ItemPage.module.css';
 
 const ItemPage = (props) => {
 
-
+    const [size, setSize] = useState({
+        widthImg: 0,
+        heightImg: 0,
+    })
     const { status, error, imgOne, author } = props.imgList;
     const imgla = props.imgLA;
+    let a = JSON.stringify(imgOne);
+    let b = a.split('"');
+    const c = b.find(i => i.includes('.'))
+
+
+    async function main() {
+        const { width, height } = await getImageSize(require('./../../../stock_back/img/' + c));
+        setSize({
+            ...size,
+            widthImg: width,
+            heightImg: height
+        })
+        console.log(width, height)
+    }
 
     useEffect(() => {
-
+        main()
     }, [props.imgList, props.imgLA])
 
 
@@ -30,6 +49,7 @@ const ItemPage = (props) => {
                                     alt="фото"
                                     onContextMenu={(e) => { e.preventDefault(); return false; }}
                                 />
+                                <p>Ширина {size.widthImg} px высота {size.heightImg} px</p>
                                 <p>Теги: {item.tags}</p>
                             </div>
                         )}
