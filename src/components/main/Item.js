b85561c86fc8c973a2d53d4dcfea939d11a0841e
unknown <sharipov.r@mail.ru>
2023-02-28 01:09:30 +0400

    image size

--- src/components/main/Item.js
+++ src/components/main/Item.js
@@ -1,25 +1,31 @@
 import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import style from './Item.module.css';
 import regExtension from '../../regexp/regExtension';
 
 
 const Item = (props) => {
 
+    const { ref, inView } = useInView({
+        threshold: 0.5
+    })
+
     useEffect(() => {
     }, [props.imgList])
 
     const imgList = props.imgList;
-
+    console.log(inView)
     return (
         <div className={style.container} >
 
             {imgList.data.map(item => <div
                 key={item.id}
+                ref={ref}
                 className={style.cardItem}
                 onClick={() => props.funcRedirect(item.id)}
             >
                 <div className={style.wrapper} >
-                    {regExtension.test(item.img_original_big) ? <img
+                 {  regExtension.test(item.img_original_big) ? <img
                         src={require('./../../../../stock_back/img/' + item.img_original_big)}
                         alt={item.id}
                         className={style.img}
@@ -29,9 +35,10 @@ const Item = (props) => {
                         className={style.img}
                         controls="controls"
                         onContextMenu={(e) => { e.preventDefault(); return false; }}
-                    ></video>}
+                    ></video> }
                 </div>
-            </div>)}
+            </div>
+            )}
         </div>
 
     );
